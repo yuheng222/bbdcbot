55a2db11e82c549f1a8b9800d8547f00a556eeab
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2019-11-24 01:00:30 +0800

    Refactored for new BBDC bot

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"io"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -17,25 +18,23 @@ import (
 )
 
 func main() {
-	//loadEnvironmentalVariables()
-
-	//log to file as well as stdout
-	f, err := os.OpenFile("output.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+	if os.Getenv("IS_HEROKU") != "TRUE" {
+		loadEnvironmentalVariables()
 	}
-	defer f.Close()
-	mw := io.MultiWriter(os.Stdout, f)
-	log.SetOutput(mw)
 
 	//set up telegram info
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
 	errCheck(err, "Failed to start telegram bot")
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-	chatID, err := strconv.ParseInt(os.Getenv("CHAT_ID"), 10, 64)
-	errCheck(err, "Failed to fetch chat ID")
+	chatIDs, err := parseChatIDList((os.Getenv("CHAT_ID")))
+	errCheck(err, "Failed to fetch chat IDs")
 
 	client := &http.Client{}
+	jar := &myjar{} // add cookie jar so you can store the session ID cookie
+	jar.jar = make(map[string][]*http.Cookie)
+	client.Jar = jar
+
+	tgclient := AlertService{Bot: bot, ReceiverIDs: chatIDs}
 
 	//for heroku
 	go func() {
@@ -44,112 +43,149 @@ func main() {
 	}()
 	for {
 		//fetching cookies
-		log.Println("Fetching cookies")
-		aspxanon, sessionID := fetchCookies()
-
-		//logging in
 		log.Println("Logging in")
-		loginForm := url.Values{}
-		loginForm.Add("txtNRIC", os.Getenv("NRIC"))
-		loginForm.Add("txtPassword", os.Getenv("PASSWORD"))
-		loginForm.Add("btnLogin", " ")
-		req, err := http.NewRequest("POST", "http://www.bbdc.sg/bbdc/bbdc_web/header2.asp",
-			strings.NewReader(loginForm.Encode()))
-		errCheck(err, "Error creating log in request")
-		req.AddCookie(aspxanon)
-		req.AddCookie(sessionID)
-		req.AddCookie(&http.Cookie{Name: "language", Value: "en-US"})
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		_, err = client.Do(req)
+		err = logIn(os.Getenv("NRIC"), os.Getenv("PASSWORD"), client)
 		errCheck(err, "Error logging in")
 
-		//fetching the booking page
-		log.Println("Fetching booking page")
-		req, err = http.NewRequest("POST", "http://www.bbdc.sg/bbdc/b-3c-pLessonBooking1.asp",
-			strings.NewReader(bookingForm().Encode()))
-		req.AddCookie(aspxanon)
-		req.AddCookie(sessionID)
-		req.AddCookie(&http.Cookie{Name: "language", Value: "en-US"})
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		errCheck(err, "Error creating get bookings request")
-		resp, err := client.Do(req)
-		errCheck(err, "Error fetching booking slots")
-		body, _ := ioutil.ReadAll(resp.Body)
-		//ioutil.WriteFile("booking.txt", body, 0644)
+		rawSlots, err := slotPage(os.Getenv("ACCOUNT_ID"), client)
+		errCheck(err, "Error getting slot page")
 
-		//parse booking page to get booking dates
-		//The data is hidden away in the following function call in the HTML page
-		//fetched:
-		//doTooltipV(event,0, "03/05/2019 (Fri)","3","11:30","13:10","BBDC");
-		log.Println("Parsing booking page")
-		foundSlot := false
-		substrs := strings.Split(string(body), "doTooltipV(")[1:]
-		for _, substr := range substrs {
-			bookingData := strings.Split(substr, ",")[0:6]
-			day := bookingData[2]
-			monthInt := day[5:7]
+		slots, err := extractSlots(rawSlots)
+		errCheck(err, "Error parsing slot page")
 
-			sessionNum := bookingData[3]
-			validSlot := false
-			if (strings.Contains(day, "Wed") || strings.Contains(day, "Fri") || strings.Contains(day, "Sat")) && (monthInt == "09" || monthInt == "10") {
-				alert("Slot available on "+day+" from "+bookingData[4]+" to "+bookingData[5], bot, chatID)
-				if sessionNum != `"1"` {
-					foundSlot = true
-					validSlot = true
-				}
-			}
-
-			if validSlot {
-				//Check if the slot found is within 10 days to determine whether to auto book
-				layout := "02/01/2006"
-				dayProper, err := time.Parse(layout, strings.Split(strings.Split(day, "\"")[1], " ")[0])
-				errCheck(err, "Error parsing date of slot")
-				daysFromNow := int(dayProper.Sub(time.Now()).Hours()/24) + 1
-
-				if daysFromNow <= 10 {
-					//if the slot is today
-					//note dayProper will be at midnight of the date given
-					//so the current time will actually ahead of the day of the slot
-					//if the slot is today, and you'll get a negative number
-					log.Printf("Entered autobook segment with daysFromNow %d and slot date %s \n", daysFromNow, day)
-					if dayProper.Sub(time.Now()).Hours() > 0 || os.Getenv("AUTOBOOK_TODAY") == "TRUE" {
-						log.Printf("Proceeded with autobook")
-
-						//need to get slot ID for auto-book
-						//strings.Split(substr, ",") returns- "BBDC"); SetMouseOverToggleColor("cell145_2") ' onmouseout='hideTip(); SetMouseOverToggleColor("cell145_2")'><input type="checkbox" id="145_2" name="slot" value="1893904" onclick="SetCountAndToggleColor('cell145_2'
-						//splitting on value= and taking the second element returns- "1893904" onclick="SetCountAndToggleColor('cell145_2'
-						//then split on " and take the second element to get 1893904
-						slotID := strings.Split(strings.Split(strings.Split(substr, ",")[6], "value=")[1], "\"")[1]
-						log.Println("Booking slot")
-						req, err = http.NewRequest("POST", "http://www.bbdc.sg/bbdc/b-3c-pLessonBookingDetails.asp",
-							strings.NewReader(paymentForm(slotID).Encode()))
-						req.AddCookie(aspxanon)
-						req.AddCookie(sessionID)
-						req.AddCookie(&http.Cookie{Name: "language", Value: "en-US"})
-						req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-						errCheck(err, "Error creating get bookings request")
-						_, err = client.Do(req)
-						errCheck(err, "Error creating booking slot")
-						log.Println("Finished booking slot")
-
-						alert("Auto-booked the slot as it was within 10 days of the current date", bot, chatID)
-					} else {
-						log.Printf("Did not proceed with autobook as time till event was %f \n", dayProper.Sub(time.Now()).Hours())
-					}
-
-				}
-			}
+		valids := validSlots(slots)
+		for _, validSlot := range valids { //for all the slots which meet the rule (i.e. within 10 days of now)
+			tgclient.MessageAll("Slot available on " + validSlot.Date.Format("_2 Jan 2006 (Mon)") + " " + os.Getenv("SESSION_"+validSlot.SessionNumber))
+		}
+		if len(valids) != 0 {
+			tgclient.MessageAll("Finished getting slots")
 		}
 
-		if foundSlot {
-			alert("Finished getting slots", bot, chatID)
-		} else {
-			log.Println("No slots found")
-		}
 		r := rand.Intn(300) + 120
 		time.Sleep(time.Duration(r) * time.Second)
 	}
 
+}
+
+// returns slots that should be autobooked/alerted about
+func validSlots(slots []DrivingSlot) []DrivingSlot {
+	valids := make([]DrivingSlot, 0)
+	for _, slot := range slots {
+		if slot.Date.Sub(time.Now()) < 10*(24*time.Hour) { // if slot is within 10 days of now
+			if slot.Date.Sub(time.Now()) > 1*(24*time.Hour) { // if slot is more than 24 hours from now
+				if slot.SessionNumber != "1" { // slot can't be today
+					valids = append(valids, slot)
+				}
+			}
+
+		}
+	}
+	return valids
+}
+
+type myjar struct {
+	jar map[string][]*http.Cookie
+}
+
+func (p *myjar) SetCookies(u *url.URL, cookies []*http.Cookie) {
+	fmt.Printf("The URL is : %s\n", u.String())
+	fmt.Printf("The cookie being set is : %s\n", cookies)
+	p.jar[u.Host] = cookies
+}
+
+func (p *myjar) Cookies(u *url.URL) []*http.Cookie {
+	fmt.Printf("The URL is : %s\n", u.String())
+	fmt.Printf("Cookie being returned is : %s\n", p.jar[u.Host])
+	return p.jar[u.Host]
+}
+
+// DrivingSlot represents a CDC slot to go for driving lessons
+type DrivingSlot struct {
+	Date          time.Time
+	SessionNumber string
+}
+
+// AlertService is a service for alerting many telegram users
+type AlertService struct {
+	Bot         *tgbotapi.BotAPI
+	ReceiverIDs []int64
+}
+
+// MessageAll sends a message to all chats in the alert service
+func (as *AlertService) MessageAll(msg string) {
+	for _, chatID := range as.ReceiverIDs {
+		alert(msg, as.Bot, chatID)
+	}
+}
+
+func parseChatIDList(list string) ([]int64, error) {
+	chatIDStrings := strings.Split(list, ",")
+	chatIDs := make([]int64, len(chatIDStrings))
+	for i, chatIDString := range chatIDStrings {
+		chatID, err := strconv.ParseInt(strings.TrimSpace(chatIDString), 10, 64)
+		chatIDs[i] = chatID
+		if err != nil {
+			return nil, err
+		}
+	}
+	return chatIDs, nil
+}
+
+func logIn(nric string, pwd string, client *http.Client) error {
+	loginForm := url.Values{}
+	loginForm.Add("txtNRIC", nric)
+	loginForm.Add("txtpassword", pwd)
+	loginForm.Add("btnLogin", "ACCESS+TO+BOOKING+SYSTEM")
+	req, err := http.NewRequest("POST", "http://www.bbdc.sg/bbdc/bbdc_web/header2.asp", strings.NewReader(loginForm.Encode()))
+	if err != nil {
+		return errors.New("Error creating request: " + err.Error())
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	_, err = client.Do(req)
+	if err != nil { // not checking for incorrect password, for fully secure version do check that in the response
+		return errors.New("Error sending request: " + err.Error())
+	}
+	return nil
+}
+
+func extractSlots(slotPage string) ([]DrivingSlot, error) {
+	// parse booking page to get booking dates
+	// The data is hidden away in the following function call in the HTML page
+	// fetched:
+	// doTooltipV(event,0, "03/05/2019 (Fri)","3","11:30","13:10","BBDC");
+
+	slotSections := strings.Split(slotPage, "doTooltipV(")[1:]
+	slots := make([]DrivingSlot, 0)
+	for _, slotSection := range slotSections {
+		bookingData := strings.Split(slotSection, ",")[0:6]
+		sessionNum := strings.Split(bookingData[3], "\"")[1] // strip of quotes
+		rawDay := bookingData[2]                             // looks like  "03/05/2019 (Fri)"
+		layout := "02/01/2006"
+		day, err := time.Parse(layout, strings.Split(strings.Split(rawDay, "\"")[1], " ")[0]) // strip of quotes and remove the `(Fri)`
+		if err != nil {
+			return nil, errors.New("Error parsing date: " + err.Error())
+		}
+		slots = append(slots, DrivingSlot{Date: day, SessionNumber: sessionNum})
+	}
+
+	return slots, nil
+}
+
+func slotPage(accountID string, client *http.Client) (string, error) {
+	req, err := http.NewRequest("POST", "http://www.bbdc.sg/bbdc/b-3c-pLessonBooking1.asp",
+		strings.NewReader(bookingForm(accountID).Encode()))
+	if err != nil {
+		return "", errors.New("Error creating request: " + err.Error())
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", errors.New("Error sending request: " + err.Error())
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	// ioutil.WriteFile("booking.txt", body, 0644)
+	return string(body), nil
 }
 
 func alert(msg string, bot *tgbotapi.BotAPI, chatID int64) {
@@ -184,14 +220,12 @@ func paymentForm(slotID string) url.Values {
 	return form
 }
 
-func bookingForm() url.Values {
+func bookingForm(accountID string) url.Values {
 	bookingForm := url.Values{}
-	bookingForm.Add("accId", os.Getenv("ACCOUNT_ID"))
-	bookingForm.Add("Month", "Feb/2019")
-	bookingForm.Add("Month", "Mar/2019")
-	bookingForm.Add("Month", "Apr/2019")
-	bookingForm.Add("Month", "May/2019")
-	bookingForm.Add("Month", "Jun/2019")
+	bookingForm.Add("accId", os.Getenv(accountID))
+	bookingForm.Add("Month", "Nov/2019")
+	bookingForm.Add("Month", "Dec/2019")
+	bookingForm.Add("Month", "Jan/2020")
 	bookingForm.Add("Session", "1")
 	bookingForm.Add("Session", "2")
 	bookingForm.Add("Session", "3")
